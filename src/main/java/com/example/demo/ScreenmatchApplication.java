ff79199f289da86f6f698c6a4446d12dc331fb27
Monique <monique.azevedo13022@gmail.com>
2025-02-19 12:57:21 -0300

    refactor: create new request

--- src/main/java/com/example/demo/ScreenmatchApplication.java
+++ src/main/java/com/example/demo/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.model.DadosEpisodio;
 import com.example.demo.model.DadosSerie;
+import com.example.demo.model.DadosTemporada;
 import com.example.demo.service.ConsumoApi;
 import com.example.demo.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -19,8 +21,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
         ConsumoApi consumoApi = new ConsumoApi();
         var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 
-        ConverteDados converteDados = new ConverteDados();
-        DadosSerie dados = converteDados.obterDados(json, DadosSerie.class);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+
+        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
+        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+        DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+        System.out.println(dadosTemporada);
     }
 }
