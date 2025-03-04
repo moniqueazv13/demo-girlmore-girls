4fb14e7b088eeef404de7f720632cdd5bdf4a28b
Monique <monique.azevedo13022@gmail.com>
2025-03-04 09:49:31 -0300

    refactor: return new data

--- src/main/java/com/example/demo/ScreenmatchApplication.java
+++ src/main/java/com/example/demo/ScreenmatchApplication.java
@@ -1,62 +1,20 @@
 package com.example.demo;
 
-import com.example.demo.model.DadosEpisodio;
-import com.example.demo.model.DadosSerie;
-import com.example.demo.model.DadosTemporada;
-import com.example.demo.service.ConsumoApi;
-import com.example.demo.service.ConverteDados;
+import com.example.demo.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(com.example.demo.ScreenmatchApplication.class, args);
+        SpringApplication.run(ScreenmatchApplication.class, args);
     }
 
     @Override
-    public void run(String... args) {
-        ConsumoApi consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
-//
-//        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-//        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-//        System.out.println(dadosEpisodio);
-//
-//        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-//        DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-//        System.out.println(dadosTemporada);
-
-        // código omitido
-
-//        System.out.println(dados);
-//        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-//        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-//        System.out.println(dadosEpisodio);
-
-        List<DadosEpisodio> dadosEpisodiosList = new ArrayList<>();
-        for (int i = 1; i <= dados.totalTemporadas(); i++) {
-            json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode="+i+"&apikey=6585022c");
-            DadosEpisodio dadosEpisodios = conversor.obterDados(json, DadosEpisodio.class);
-            dadosEpisodiosList.add(dadosEpisodios);
-        }
-        dadosEpisodiosList.forEach(System.out::println);
-
-List<DadosTemporada> dadosTemporadaList = new ArrayList<>();
-        for(int i = 1; i<=dados.totalTemporadas(); i++) {
-            json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            dadosTemporadaList.add(dadosTemporada);
-        }
-        dadosTemporadaList.forEach(System.out::println);
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.exibeMenu();
     }
 }
---
+++ src/main/java/com/example/demo/principal/Principal.java
@@ -0,0 +1,55 @@
+package com.example.demo.principal;
+
+import com.example.demo.model.DadosEpisodio;
+import com.example.demo.model.DadosSerie;
+import com.example.demo.model.DadosTemporada;
+import com.example.demo.service.ConsumoApi;
+import com.example.demo.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+//        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (DadosEpisodio dadosEpisodio : episodiosTemporada) {
+//                System.out.println(dadosEpisodio.titulo());
+//            }
+//        }
+
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .toList();
+
+//        dadosEpisodios.add(new DadosEpisodio("teste", 3, "10", "2020-01-01"));
+        dadosEpisodios. forEach(System.out::println);
+    }
+}
